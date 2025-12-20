3b16946d40a5ac46cc55bffdcbe605e3daa954e2
worldlightyjx <worldlightyjx@qq.com>
2025-12-20 14:31:52 +0800

    repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        while (true){
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            System.out.println(command+": command not found");
+        }
         // TODO: Uncomment the code below to pass the first stage
-         System.out.print("$ ");
-         Scanner scanner = new Scanner(System.in);
-         String command = scanner.nextLine();
-         System.out.println(command+": command not found");
+
     }
 }
